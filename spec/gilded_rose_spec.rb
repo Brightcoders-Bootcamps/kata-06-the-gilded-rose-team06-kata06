35449b9d86d606b347e08a10b89b1180320fcd0f
glorianayeli <glorianayeli_jaime@ucol.mx>
2021-01-11 17:29:15 -0600

    add the test for the backstage author: glorianayeli <glorianayeli_jaime@ucol.mx

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,16 +1,21 @@
 require File.join(File.dirname(__FILE__), '../app/gilded_rose')
 require File.join(File.dirname(__FILE__), '../app/item_class')
 
-describe GildedRose do
-
+RSpec.describe GildedRose do
   before do
-    @Item = Item.new("Sulfuras, Hand of Ragnaros",0,5)
-    @GildedRose = GildedRose.new(@Item)
+    @item = Item.new("Sulfuras, Hand of Ragnaros",0,5)
+    @gilded_rose = GildedRose.new(@item)
   end
   describe ".update_quality_by_type" do
     it "doesn't change quality and can't be sell" do
-      expect(@GildedRose.update_quality_by_type(@Item)).to eq(false)
+      expect(@gilded_rose.update_quality_by_type(@item)).to eq(false)
     end
+    it "Backstage passes, increases in Quality as its SellIn value approaches" do
+      @item.name = "Backstage passes to a TAFKAL80ETC concert";
+      @item.sell_in = 8;
+      @item.quality = 5;
+      expect(@gilded_rose.update_quality_by_type(@item)).to eq(7)
+    end
+    
   end
-
 end
