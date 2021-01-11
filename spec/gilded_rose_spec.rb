dc6632649e5e24d30204c017d86b44743cffc2e8
Jose Alberto Negrete Gonzalez <jnegrete15@ucol.mx>
2021-01-11 17:31:27 -0600

    Test about Aged brie items and conjured
    To test them run the gilded_rose_spec.rb file using the rspec command
    
    Author: jnegrete15

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,16 +1,42 @@
 require File.join(File.dirname(__FILE__), '../app/gilded_rose')
 require File.join(File.dirname(__FILE__), '../app/item_class')
 
-describe GildedRose do
-
+RSpec.describe GildedRose do
   before do
-    @Item = Item.new("Sulfuras, Hand of Ragnaros",0,5)
-    @GildedRose = GildedRose.new(@Item)
+    @item = Item.new("Sulfuras, Hand of Ragnaros",0,5)
+    @gilded_rose = GildedRose.new(@item)
   end
   describe ".update_quality_by_type" do
     it "doesn't change quality and can't be sell" do
-      expect(@GildedRose.update_quality_by_type(@Item)).to eq(false)
+      expect(@gilded_rose.update_quality_by_type(@item)).to eq(false)
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+    it "Aged Brie increase the quality" do
+      @item.name = "Aged Brie";
+      @item.sell_in =5;
+      @item.quality =7;
+      expect(@gilded_rose.update_quality_by_type(@item)).to eq(8)
+    end
+
+    it "Conjured, decrease quality twice" do
+      @item.name = "Conjured Mana Cake";
+      @item.sell_in = 8;
+      @item.quality = 5;
+      expect(@gilded_rose.update_quality_by_type(@item)).to eq(3)
     end
   end
-
 end
